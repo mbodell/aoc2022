a18a9ad3c357340e1acec70b90cea9cc98b3a33f
mbodell <mbodell@gmail.com>
2022-12-31 00:21:31 -0800

    Solved day4

---
+++ day04/index.js
@@ -0,0 +1,19 @@
+const lineReader = require("line-reader");
+const Promise = require('bluebird');
+
+const eachLine = Promise.promisify(lineReader.eachLine);
+
+let filename = process.argv.slice(2)[0] || 'input.txt';
+
+let answer = 0;
+
+eachLine(filename, function(line) {
+  let e = line.split(",").map(s=>s.split("-").map(s=>parseInt(s)));
+  if(e[0][0]>=e[1][0] && e[0][1]<=e[1][1]) {
+    answer++;
+  } else if (e[1][0]>=e[0][0] && e[1][1]<=e[0][1]) {
+    answer++;
+  } 
+}).then(function(err) {
+  console.log(answer);
+});
